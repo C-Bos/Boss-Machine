cce9edde97ba7ca04edde89d630466ab04359105
C <67757576+C-Bos@users.noreply.github.com>
2021-01-21 15:31:40 +0000

    Added checkMillionDollarIdea function

--- project-4-boss-machine-start/project-4-boss-machine-start/server/checkMillionDollarIdea.js
+++ project-4-boss-machine-start/project-4-boss-machine-start/server/checkMillionDollarIdea.js
@@ -1,4 +1,14 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+    //This function will make sure that any new or updated ideas are still worth at least one million dollars!
+    //The total value of an idea is the product of its numWeeks and weeklyRevenue properties.
+    const { numWeeks, weeklyRevenue } = req.body;
+    const totalValue = Number(numWeeks) * Number(weeklyRevenue);
+    if (totalValue >= 1000000) {
+        next();
+    } else {
+        res.status(400).send();
+    }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
