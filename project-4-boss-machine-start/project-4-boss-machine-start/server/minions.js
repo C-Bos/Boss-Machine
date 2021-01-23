6923c5c60bbaf61bfe4ceb0bb3bb088bd8ac5bdf
C <67757576+C-Bos@users.noreply.github.com>
2021-01-23 18:08:21 +0000

    Bonus routes added. Final project! Passed all test

--- project-4-boss-machine-start/project-4-boss-machine-start/server/minions.js
+++ project-4-boss-machine-start/project-4-boss-machine-start/server/minions.js
@@ -56,3 +56,52 @@ minionsRouter.delete('/:minionId', (req, res, next) => {
     res.send();
   });
 
+  //BONUS routes
+
+minionsRouter.param('/workId', (req, res, next, id) => {
+    const work = getFromDatabaseById('work', id);
+    if (work) {
+        req.work = work;
+        next();
+    } else {
+        res.status(404).send();
+    }
+});
+
+//   GET /api/minions/:minionId/work to get an array of all work for the specified minon.
+minionsRouter.get('/:minionId/work', (req, res, next) => {
+    const work = getAllFromDatabase('work').filter((singleWork) => {
+        return singleWork.minionId === req.params.minionId;
+    });
+    res.send(work);
+});
+
+//   POST /api/minions/:minionId/work to create a new work object and save it to the database.
+minionsRouter.post('/:minionId/work', (req, res, next) => {
+    const workToAdd = req.body;
+    workToAdd.minionId = req.params.minionId;
+    const createdWork = addToDatabase('work', workToAdd);
+    res.status(201).send(createdWork);
+  });
+
+//   PUT /api/minions/:minionId/work/:workId to update a single work by id.
+minionsRouter.put('/:minionId/work/:workId', (req, res, next) => {
+    if(req.params.minionId !== req.body.minionId) {
+        res.status(400).send();
+    } else {
+        updatedWork = updateInstanceInDatabase('work', req.body);
+        res.send(updatedWork);
+    }
+});
+
+//   DELETE /api/minions/:minionId/work/:workId to delete a single work by id.
+minionsRouter.delete('/:minionId/work/:workId', (req, res, next) => {
+    const successfullyDeleted = deleteFromDatabasebyId('work', req.params.workId);
+    if(successfullyDeleted) {
+        res.status(204);
+    } else {
+        res.status(500);
+    } 
+    res.send();
+  });
+
